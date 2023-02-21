ceebf68df6449a22a90fb2b47bfcc6d5ece76a76
VitaNYCH <vitanychvoloda@yahoo.com>
2023-02-21 23:37:33 +0200

    Added gallery with pictures

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const picturesContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+picturesContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return galleryItems.map(({ preview, original, description })=> {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  }).join('');
+}
+
