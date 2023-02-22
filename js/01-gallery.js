aa51853f99d1e418699f611671c8d4e7c5401e78
VitaNYCH <vitanychvoloda@yahoo.com>
2023-02-22 22:43:21 +0200

    Added library for modal window

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-
 const picturesContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
@@ -27,8 +24,22 @@ function createGalleryItemsMarkup(items) {
 }
 
 function onPicturesContainerClick(event) {
+  event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
   console.log(event.target.dataset.source);
+
+  const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}' width="800" height="600">
+`);
+
+  instance.show();
+
+  picturesContainer.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
 }
+
